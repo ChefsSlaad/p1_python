1d752633ed49ba42481d16386cc368ef8fb282e0
Marc Wagner <wagner.marc@gmail.com>
2019-01-01 13:36:51 +0100

    fine-tuning

--- p1_reader.py
+++ p1_reader.py
@@ -23,10 +23,11 @@ def main():
     pwr      = datagram['tarif_1_delivered'] + datagram['tarif_2_delivered']
     pwr_old  = pwr
     pwr_time = datagram['date_time_str']
-    pwr_avg  = None
-    gas_avg  = None
+    pwr_avg  = '-'
+    gas_avg  = '-'
 
-    while True:   
+    while True:
+        print('.' , end = '')
         if time()-secs > max_time: 
             datagram = read_datagram(next(read_telegram()))
             if pwr_time != datagram['date_time_str']:
@@ -38,14 +39,13 @@ def main():
                 gas      = datagram['gas_delivered']
                 gas_avg  = round((gas - gas_old)*(3600000/300),3) #gas refresh is 5 min
                 gas_old = gas
-
-             datagram['power_avg'] = pwr_avg # either None, old value or new value, dept on update
-             datagram['gas_avg'] = gas_avg   # either None, old value or new value, dept on update
+            datagram['power_avg'] = pwr_avg # either None, old value or new value, dept on update
+            datagram['gas_avg'] = gas_avg   # either None, old value or new value, dept on update
 #            print('{:3} current_power {:10} last_power {:10} diff {}'.format(round(time()-secs), pwr  , pwr_old, pwr_avg))
 #            print('{:3} current gas   {:10} last gas   {:10} diff {}'.format(round(time()-secs),gas, gas_old, gas_avg))
 #            print(round(time()-secs), 'gas', gas_avg, 'power', pwr_avg)
 #            print('gas {:10} power {:10}'.format(datagram['gas_avg'], datagram['power_avg']))
-            print('{} total power {} total gas {} power used {} gas used {}'.format(datagram['date_time_str'], pwr, gas, pwr_avg, gas_avg)) 
+            print('\n{} total power {} total gas {} power used {} gas used {}'.format(datagram['date_time_str'], pwr, gas, pwr_avg, gas_avg)) 
             mqtt_client.publish(power_topic, dumps(datagram))
             secs = time()
         sleep(1)
